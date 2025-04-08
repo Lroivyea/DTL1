d58b108aa1e26879a0201b92f4309140f0838ac7
Khush Gandhi <khush@Khushs-MacBook-Air.local>
2025-04-08 14:30:02 +0530

    Multiplication added

--- calculator.py
+++ calculator.py
@@ -28,6 +28,12 @@ def hex_calculator():
     else:
         print("Invalid choice")
 
-hex_calculator()
+def hex_mul():
+    a = 0x46
+    b = 0x31
+    print(f"Result: {(a * b)}")
+    return
 
+hex_calculator()
+hex_mul()
 
